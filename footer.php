a6fbe5f41c10f3f2a3d9842b3d3a860cd000b20d
essadiki <si1981mo@gmail.com>
2016-12-08 18:32:17 +0000

    my first commit

---
+++ footer.php
@@ -0,0 +1,23 @@
+
+      <footer>
+        <p>جميع الحقوق محفوظة &copy; موقع صديق المطور العربي-قالب من تصميم صديق المطور العربي باستعمال Bootstrap</p>
+      </footer>
+    </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+   <!--  <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script> -->
+    
+    <script src="<?php echo get_template_directory_uri () ?>/js/bootstrap-arabic.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+   <!--  <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>  -->
+    
+<?php
+wp_footer();
+?>
+
+  </body>
+</html>
